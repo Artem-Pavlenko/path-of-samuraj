a226d2fb11d017d8ddea4f6c41277298382f9548
Artem <rozen19942810@gmail.com>
2020-11-19 18:44:40 +0200

    Updates

---
+++ precache-manifest.a8d9b1b9fed9c09052441af6e4784cf4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d316db8f05783159782ceb131e80cc61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f848618648f7059a7622",
+    "url": "/static/css/3.cf26ed72.chunk.css"
+  },
+  {
+    "revision": "d21c7b2cb65abf9f0db3",
+    "url": "/static/css/4.f27c4de5.chunk.css"
+  },
+  {
+    "revision": "a32801c82bfdd75c2fd3",
+    "url": "/static/css/main.fd898d56.chunk.css"
+  },
+  {
+    "revision": "4cf6879d1807abd845c0",
+    "url": "/static/js/2.202aa9a6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.202aa9a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f848618648f7059a7622",
+    "url": "/static/js/3.f752ba85.chunk.js"
+  },
+  {
+    "revision": "d21c7b2cb65abf9f0db3",
+    "url": "/static/js/4.b61ce27b.chunk.js"
+  },
+  {
+    "revision": "a32801c82bfdd75c2fd3",
+    "url": "/static/js/main.edcd4aa2.chunk.js"
+  },
+  {
+    "revision": "ee2c12988bcfca6c493b",
+    "url": "/static/js/runtime-main.5c8964e3.js"
+  },
+  {
+    "revision": "97f0a69e36ed14bdcdb37605ea0348fb",
+    "url": "/static/media/fsociety-mask-549635.97f0a69e.png"
+  },
+  {
+    "revision": "fa3b73e48a8e6af78ff364f4a3a7bf2e",
+    "url": "/static/media/rss.fa3b73e4.png"
+  }
+]);
